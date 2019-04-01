fbad108e4ca1ad27710407e938c2823263be7823
elbertkreukniet <elbert@kreukniet.com>
2019-04-01 13:13:47 +0200

    Remove unused macro

--- Common.h
+++ Common.h
@@ -6,8 +6,4 @@
 
 using Eigen::MatrixXd;
 
-#define assertValue(message, expected, value) \
-    std::cout << message << ": " << value << " (" << expected << ")" << std::endl; \
-    assert(abs((expected - value) / (expected + value)) < 1E-4);
-
 MatrixXd load_csv(const std::string &path);
